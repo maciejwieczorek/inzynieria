fbbb2b2c044564ad4a22d60de96feb5ff9eefbec
maciejwieczorek <36855045+maciejwieczorek@users.noreply.github.com>
2018-03-12 17:11:01 +0100

    Add files via upload

---
+++ lekcja_3/TStudent.cpp
@@ -0,0 +1,49 @@
+/**
+ * Project Untitled
+ */
+
+
+#include "TStudent.h"
+
+/**
+ * TStudent implementation
+ */
+
+
+void TStudent::TStudent() {
+
+}
+
+void TStudent::TStudent() {
+
+}
+
+/**
+ * @param name
+ * @return void
+ */
+void TStudent::setName(string name) {
+    return;
+}
+
+/**
+ * @return string
+ */
+string TStudent::getName() {
+    return "";
+}
+
+/**
+ * @param age
+ * @return void
+ */
+void TStudent::setAge(int age) {
+    return;
+}
+
+/**
+ * @return int
+ */
+int TStudent::getAge() {
+    return 0;
+}
---
+++ lekcja_3/TUniversity.cpp
@@ -0,0 +1,44 @@
+/**
+ * Project Untitled
+ */
+
+
+#include "TUniversity.h"
+
+/**
+ * TUniversity implementation
+ */
+
+
+/**
+ * @param st
+ * @return void
+ */
+void TUniversity::attach(TStudent* st) {
+    return;
+}
+
+/**
+ * @return void
+ */
+void TUniversity::detach() {
+    return;
+}
+
+/**
+ * @param i
+ * @return void
+ */
+void TUniversity::printData(int i) {
+    return;
+}
+
+/**
+ * @param i
+ * @param name
+ * @param age
+ * @return void
+ */
+void TUniversity::setData(int i, string name, int age) {
+    return;
+}
